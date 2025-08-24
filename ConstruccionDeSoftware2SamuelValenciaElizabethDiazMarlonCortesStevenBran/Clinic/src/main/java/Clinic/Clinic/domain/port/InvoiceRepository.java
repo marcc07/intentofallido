82cb5b1d2b3bd7663c1fb5191081e4d5aceb4e44
marcc07 <marlonalexiscortes@gmail.com>
2025-08-23 19:53:58 -0500

    Delete ConstruccionDeSoftware2SamuelValenciaElizabethDiazMarlonCortesStevenBran directory

--- ConstruccionDeSoftware2SamuelValenciaElizabethDiazMarlonCortesStevenBran/Clinic/src/main/java/Clinic/Clinic/domain/port/InvoiceRepository.java
+++
@@ -1,9 +0,0 @@
-package Clinic.Clinic.domain.port;
-
-import Clinic.Clinic.domain.model.Invoice;
-import java.util.Optional;
-
-public interface InvoiceRepository {
-    Optional<Invoice> findById(String id);
-    Invoice save(Invoice invoice);
-}
--- ConstruccionDeSoftware2SamuelValenciaElizabethDiazMarlonCortesStevenBran/Clinic/src/main/java/Clinic/Clinic/domain/port/LedgerRepository.java
+++
@@ -1,9 +0,0 @@
-package Clinic.Clinic.domain.port;
-
-import Clinic.Clinic.domain.model.CopayLedger;
-import java.util.Optional;
-
-public interface LedgerRepository {
-    Optional<CopayLedger> findByPatientAndYear(String patientId, int year);
-    CopayLedger save(CopayLedger ledger);
-}
